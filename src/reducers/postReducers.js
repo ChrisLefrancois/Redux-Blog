8672c1702f91ee5069f3bf87564c6c320950b79b
Christopher Lefrancois <chris.lefrancois1@gmail.com>
2022-10-19 13:12:55 -0400

    almost done

--- src/reducers/postReducers.js
+++ src/reducers/postReducers.js
@@ -1,3 +1,11 @@
-export default () => {
-  return 123;
+import PostList from "../components/PostList";
+
+const Posts = (state = [], action) => {
+  if (action.type === 'FETCH_POSTS') {
+    return action.payload;
+  }
+
+  return state;
 };
+
+export default Posts;
